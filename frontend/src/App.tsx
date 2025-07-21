bcc96708b9f78427542baa50bdad90f55e55f912
Georgios Kondylis <gk@Georgioss-MBP.lan>
2025-07-21 13:19:19 +0200

    started working with the anime Section fixing fallback array because the opular series has only 3 irelevant anime

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,19 @@ import Navbar from './components/Navbar/Navbar'
 import { Home, Series, Loader } from './components/exports'
 import { useLocation } from 'react-router-dom'
 import Navbar4Kids from './components/Navbar/Navbar4Kids'
+import Anime from './components/Anime/Anime'
 
 const App = () => {
   const location = useLocation();
   return (
     <>
-      <Loader />
+      {/* <Loader /> */}
       {location.pathname === '/4kids' ? <Navbar4Kids /> : <Navbar />}
     
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/series" element={<Series />} />
-        <Route path="/anime" element={<div>Services Page</div>} />
+        <Route path="/anime" element={<Anime />} />
         <Route path="/pricing" element={<div>Portfolio Page</div>} />
         <Route path="/contact" element={<div>Contact Page</div>} />
       </Routes>
---
+++ frontend/src/components/Anime/AnimeSeriesInfo.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import type { Serie } from "../../constants/types";
+import StarRating from "../ui/StarRating";
+import { useGlobalProps } from "../../GlobalContext";
+
+type Props = { currentSeries: Serie };
+
+const AnimeSeriesInfo = ({ currentSeries }: Props) => {
+  const { customStyles } = useGlobalProps();
+
+  return (
+    <div className="flex flex-col gap-4 text-white">
+      {/* Series Title */}
+      <p className="text-[40px]">{currentSeries.name}</p>
+
+      {/* Overview with dash cleaning */}
+      <div className="txtShadowBlack max-w-[600px] w-full rounded-[13px]">
+        <p>{currentSeries.overview.replace(/–/g, "")}</p>
+      </div>
+
+      {/* Rating */}
+      <div className="flex w-fit p-2 gap-2 text-[20px] rounded-[10px] bg-[#80808043] backdrop-blur-[3px]">
+        <StarRating rating={currentSeries.vote_average / 2} />
+        <span className="text-[15px] text-[#e1e1e1cd]">
+          {(currentSeries.vote_average / 2).toFixed(1)} / 5 &nbsp; (
+          {currentSeries.vote_count.toLocaleString()} votes )
+        </span>
+      </div>
+
+      {/* Buttons */}
+      <div className="flex gap-2 items-center">
+        <button
+          className={`${customStyles?.btnColor} flex gap-2 items-center px-[12px] py-[6px] rounded-[7px] hover:text-[#c5c5c5] cursor-pointer`}
+        >
+          <i className="fa-solid fa-play"></i>
+          Watch Trailer
+        </button>
+        <button
+          className={`border flex gap-[7px] items-center px-[11px] py-[5px] rounded-[7px] hover:gap-[10px] cursor-pointer transition1`}
+        >
+          See more
+          <i className="fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AnimeSeriesInfo;
