da0f4446cb3ee80cc259aee4536fd1b5b826c238
Georgios Kondylis <gk@Georgioss-MBP.lan>
2025-08-10 21:49:30 +0200

    fixed a ui

--- frontend/src/components/AUTH/profiles/ViewProfileCards/FavouriteCard.tsx
+++ frontend/src/components/AUTH/profiles/ViewProfileCards/FavouriteCard.tsx
@@ -12,7 +12,7 @@ const FavouriteCard = ({ movie } : Props) => {
 
   return (
     <Link to={isMovie ? `/${movie.id}` : `/series/${movie.id}`} 
-      className="flex rounded-lg shadow hover:scale-105 transition1 min-w-[150px] max-sm:min-w-[80px] w-[150px] px-[5px]"
+      className="flex rounded-lg shadow hover:scale-105 transition1 min-w-[150px] max-sm:min-w-[80px] max-sm:max-w-[80px] w-[150px] px-[5px]"
     >
       <img
         src={`${IMAGE_BASE}${movie.poster_path}`}
